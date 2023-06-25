376140d437df935da30a8e0915d17b216f7813a3
Leonardo Gatti <leonss23esp@gmail.com>
2023-06-25 11:09:34 -0300

    [ADD] `sort.js` with `bubbleSort` and `selectionSort`

--- sort.js
+++ sort.js
@@ -1,14 +1,37 @@
-const { benchmark } = require("./utils");
+const { benchmark, generateInverseArray } = require("./utils");
 
+/// O(n^2)
 function bubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {}
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minIdx = i;
+    for (let j = i; j < arr.length - 1; j++) {
+      if (arr[i] < minIdx) {
+        minIdx = i;
+      }
+    }
+    [arr[i], arr[minIdx]] = [arr[minIdx], arr[i]];
+  }
+  return arr;
 }
 
 // Test Data
 
-let arr = [];
+let arr = generateInverseArray(16);
+console.log(arr);
 
 // Tests/Benchmarks
 let resultBubbleSort = benchmark(bubbleSort, [arr]);
+let resultSelectionSort = benchmark(selectionSort, [arr]);
 
-console.log({ resultBubbleSort });
+console.log({ resultBubbleSort, resultSelectionSort });
