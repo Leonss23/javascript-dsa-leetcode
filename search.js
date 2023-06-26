496aff4925b46c90645cbf9db94e6462c7f1423e
Leonardo Gatti <leonss23esp@gmail.com>
2023-06-26 00:17:41 -0300

    [UPDATE] `search.js` functions now use iteration instead of recursion.

--- search.js
+++ search.js
@@ -10,33 +10,42 @@ function linearSearch(arr, target) {
 
 /// O(log(n))
 function binarySearch(arr, target, low = 0, high = arr.length - 1) {
-  let probe = Math.floor(low + (high - low) / 2);
+  while (low <= high) {
+    let probe = Math.floor(low + (high - low) / 2);
 
-  if (arr[probe] == target) return probe;
-  if (arr[probe] < target) return binarySearch(arr, target, ++probe, high);
-  if (arr[probe] > target) return binarySearch(arr, target, low, --probe);
+    if (arr[probe] === target) return probe;
+    if (arr[probe] < target) low = probe + 1;
+    if (arr[probe] > target) high = probe - 1;
+  }
   return -1;
 }
 
 /// O(log(log(n)))
 function interpolationSearch(arr, target, low = 0, high = arr.length - 1) {
-  let probe = Math.floor(
-    low + ((high - low) * (target - arr[low])) / (arr[high] - arr[low])
-  );
+  while (low <= high) {
+    let probe = Math.floor(
+      low + ((high - low) * (target - arr[low])) / (arr[high] - arr[low])
+    );
 
-  if (arr[probe] === target) return probe;
-  if (arr[probe] < target) interpolationSearch(arr, target, ++probe);
-  if (arr[probe] > target) interpolationSearch(arr, target, low, --probe);
+    if (arr[probe] === target) return probe;
+    if (arr[probe] < target) low = probe + 1;
+    if (arr[probe] > target) high = probe - 1;
+  }
+  return -1;
 }
 
 // Test data
-let arr = generateArray(30);
+let arr = generateArray(24);
 
 let target = Math.floor(arr.length * 0.76);
 
 // Test/Benchmarks
-let foundLinearSearch = benchmark(linearSearch, [arr, target]);
+// let foundLinearSearch = benchmark(linearSearch, [arr, target]);
 let foundBinarySearch = benchmark(binarySearch, [arr, target]);
 let foundInterpolationSearch = benchmark(interpolationSearch, [arr, target]);
 
-console.log({ foundLinearSearch, foundBinarySearch, foundInterpolationSearch });
+console.log({
+  // foundLinearSearch,
+  foundBinarySearch,
+  foundInterpolationSearch,
+});
