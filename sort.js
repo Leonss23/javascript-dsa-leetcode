fb66158b4cbd70756791b43651931969da6eb885
Leonardo Gatti <leonss23esp@gmail.com>
2023-06-26 00:15:55 -0300

    [UPDATE] removed some logging from `insertionSort` function.

--- sort.js
+++ sort.js
@@ -29,10 +29,7 @@ function selectionSort(arr) {
 /// O(n^2)
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    // console.log({ i });
-
     for (let j = i; j > 0; j--) {
-      // console.log({ j });
       if (arr[j - 1] > arr[j]) {
         [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
       }
